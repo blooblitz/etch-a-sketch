06b40e021f6b1571ed7a81b5a4596301b8d5de83
blooblitz <blooponi@gmail.com>
2022-11-04 01:25:44 -0600

    Implement reset, color change, and erase. Some code cleaning.

--- javascript.js
+++ javascript.js
@@ -1,82 +1,18 @@
+const btnDarkMode = document.querySelector(".header > #dark-mode ");
+const btnReset = document.querySelector(".reset");
+const btnColor = document.querySelector("#select-color");
+const btnErase = document.querySelector(".erase");
+const btnShade = document.querySelector(".shade");
+const btnLighten = document.querySelector(".lighten");
+const grid = document.querySelector(".grid-draw-container");
+
+let darkMode = false;
 let currentRows = 32;
 let currentColumns = 32;
-const grid = document.querySelector(".grid-draw-container");
-let darkMode = false;
-const btnDarkMode = document.querySelector(".header > #dark-mode ");
+let drawColor = "#000000";
 let slider = document.getElementById("myRange");
 let output = document.getElementById("size");
-output.innerHTML = currentRows;
-slider.value = currentRows;
-
-
-btnDarkMode.addEventListener("click", () => {
-    if (darkMode) {
-        document.querySelector(".main-flex-wrapper").style = "background-color: rgb(229, 231, 235)";
-        darkMode = false;
-    } else {
-        document.querySelector(".main-flex-wrapper").style = "background-color: rgb(2, 7, 17)";
-        darkMode = true;
-    }
-});
-
-// Using oninput to get immediate feedback of grid size.
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
-
-// Using onchange to only change the grid size when the focus is lost on the slider.
-slider.onchange = function() {
-    //updateGrid(this.value, this.value);
-    deleteGrid();
-    createGrid(this.value, this.value);
-}
-
-/*
-    CREATE MAP
-*/
-/*
-let gridMap = new Map();
-function fillMap(column, row) {
-    for (let x = 0; x < column; x++) {
-        for (let y = 0; y < row; y++) {
-            let gridElement = document.createElement('div');
-            let randomColor = Math.floor(Math.random()*16777215).toString(16);
-            gridElement.className = "grid-element"
-            gridElement.id = `${x}-${y}`;
-            gridElement.style = `background-color: #${randomColor}; border: 1px solid black; border-right: 1px; border-top: 1px`;
-            gridElement.style.gridColumn = x + 1;
-            gridElement.style.gridRow = y + 1;
-            gridMap.set(`${x}-${y}`, gridElement);
-            grid.appendChild(gridElement);
-        }
-    }
-}*/
-
-//fillMap(100, 100);
-createGrid(currentColumns, currentRows);
-
-
-/*
-function updateGrid(column, row) {
-    if (column > currentColumns) {
-        for (let x = currentColumns; x < column; x++) {
-            for (let y = currentRows; y < row; y++) {
-                let gridElement = gridMap.get(`${x}-${y}`);
-                grid.appendChild(gridElement);
-            }
-        }
-    }
-    else if (column < currentColumns) {
-        for (let x = 0; x < currentColumns; x++) {
-            for (let y = currentRows - 1; y > row; y--) {
-            
-                let selector = `${x}-${y}`;
-            }
-        }
-    }
-    currentColumns = column;
-    currentRows = row;
-}*/
+let mouseDown = false;
 
 
 /*
@@ -86,34 +22,34 @@ function updateGrid(column, row) {
 */
 function createGrid(column, row) {
     let gridElement;
-    let randomColor;
-    randomColor = Math.floor(Math.random()*16777215).toString(16);
-    grid.addEventListener('mouseover', function(e){
-        if(e.target.className !== "grid-draw-container") { //There's probably a better way to do this. The event listener for the draw container must be disabled, otherwise the whole thing occasionally gets filled in.
-            e.target.style = 'background-color: gray; border: 1px solid black; border-right: 1px; border-top: 1px';
-        }
-        //e.target.style = `gridElement.style = background-color: black; border: 1px solid black; border-right: 1px; border-top: 1px`;
-    });
-    
     for (let x = 0; x < column; x++) {
         for (let y = 0; y < row; y++) {
             gridElement = document.createElement('div');
             gridElement.className = "grid-element"
-            gridElement.id = `${x}-${y}`;
-            gridElement.style = "border: 1px solid black; border-right: 1px; border-top: 1px";
-            gridElement.style.gridColumn = x + 1;
+            //gridElement.id = `${x}-${y}`;
+            gridElement.style = "background-color: white; border: 1px solid black; border-right: 1px; border-top: 1px";
             gridElement.style.gridRow = y + 1;
+            gridElement.style.gridColumn = x + 1;
+            gridElement.addEventListener('mouseover', draw);
+            gridElement.addEventListener('mousedown', draw);
             grid.appendChild(gridElement);
-            //debug
-            //console.log(gridElement.getAttribute("class"));
-            //console.log(gridElement.getAttribute("id"));
         }
     }
 }
 
+/*
+    Deletes the entire grid by remove every single child <div> from the container.
+*/
 function deleteGrid() {
     while (grid.lastChild) {
         grid.removeChild(grid.lastChild);
+    }
+}
+
+function reset() {
+    let elems = document.querySelectorAll(".grid-element");
+    for (let i = 0; i < elems.length; i++){
+        elems[i].style.backgroundColor = "white";
     }
 }
 
@@ -127,3 +63,64 @@ function validateInput(input) {
     return ~~input; //Eliminates decimals
 }
 
+function changeColor(e) {
+    drawColor = e.target.value;
+}
+
+function draw(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = drawColor;
+    
+}
+
+function shade() {
+
+}
+
+function lighten() {
+
+}
+
+function erase() {
+    drawColor = "white";
+}
+
+function toggleGrid() {
+    //swap out classs for grid and gridelements
+}
+
+function setup() {
+    //Initializes a grid on site load.
+    createGrid(currentColumns, currentRows);
+    output.innerHTML = currentRows;
+    slider.value = currentRows;
+    grid.addEventListener("mousedown", () => mouseDown = true);
+    grid.addEventListener("mouseup", () => mouseDown = false);
+    btnReset.addEventListener("click", reset);
+    btnColor.addEventListener("change", changeColor, false);
+    btnErase.addEventListener("click", erase);
+    btnDarkMode.addEventListener("click", () => {
+        if (darkMode) {
+            document.querySelector(".main-flex-wrapper").style = "background-color: rgb(229, 231, 235)";
+            darkMode = false;
+        } else {
+            document.querySelector(".main-flex-wrapper").style = "background-color: rgb(2, 7, 17)";
+            darkMode = true;
+        }
+    });
+
+    // Using oninput to get immediate feedback of grid size.
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
+    // Using onchange to only change the grid size when the focus is lost on the slider.
+    slider.onchange = function() {
+        deleteGrid();
+        createGrid(this.value, this.value);
+    }    
+}
+
+window.onload = () => {
+    setup();
+}
